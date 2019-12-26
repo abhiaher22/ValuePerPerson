8fdc59abd1b60b139a64b4f68deee3f40ee5af50
abhiaher22 <abhiaher22@gmail.com>
2019-12-26 15:02:52 -0800

    Initial Commit

---
+++ ValuePerPerson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValuePerPerson
+//
+//  Created by Abhijeet Aher on 12/26/19.
+//  Copyright © 2019 Abhijeet Aher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
