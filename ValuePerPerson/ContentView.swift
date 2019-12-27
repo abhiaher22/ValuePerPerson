f2bd8eada009954efe8b5297718d1d87234af3f6
abhiaher22 <abhiaher22@gmail.com>
2019-12-26 16:29:10 -0800

    Phase 1

--- ValuePerPerson/ContentView.swift
+++ ValuePerPerson/ContentView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    @State private var tipPercentageValue = [10,15,20,30,0]
+
+    private var perPesrsonCount : Double {
+        
+        let totalPeople = Double (numberOfPeople + 2)
+        let totalAmount = Double (checkAmount) ?? 0
+        let totalTipPercentage = Double (tipPercentageValue[tipPercentage])
+
+        let tipOfAmount = totalAmount / 100 * totalTipPercentage
+        let tipWithAmount = tipOfAmount + totalAmount
+        let perPersonShare = tipWithAmount / totalPeople
+        return perPersonShare
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+        Form{
+            Section{
+                TextField("Enter the amount: ", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+                
+                    Picker( selection: $numberOfPeople, label: Text("Number of people:" )) {
+                    ForEach(2..<100){
+                        Text("\($0) people").id($0)
+                    }
+                }
+            }
+            
+            Section(header: Text("Tip percentage")){
+                Picker(selection:$tipPercentage , label: Text("Tip Percentage: "))
+                    {
+                        ForEach(0 ..< tipPercentageValue.count){
+                            Text("\(self.tipPercentageValue[$0]) %")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section{
+                Text("$\(perPesrsonCount, specifier: "%.2f" )")
+                    }
+            }.navigationBarTitle("Per Person")
+        }
     }
 }
 
